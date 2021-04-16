5e038ab5458e6c82ed40e04c3f5b5b5d2dfebb00
dhmin5693 <dhmin5693@naver.com>
2021-04-17 02:32:47 +0900

    2021-04-17

---
+++ LeetCode/[LeetCode 20] Valid Parentheses.java
@@ -0,0 +1,74 @@
+import java.util.List;
+import java.util.Stack;
+
+class Solution {
+
+    private final static List<Character> OPENER = List.of('(', '{', '[');
+
+    public boolean isValid(String s) {
+
+        var st = new Stack<Character>();
+
+        for (char c : s.toCharArray()) {
+            if (isOpener(c)) {
+                st.push(c);
+                continue;
+            }
+
+            if (st.isEmpty()) {
+                return false;
+            }
+
+            char top = st.peek();
+
+            if (c == ')' && top == '(') {
+                st.pop();
+                continue;
+            }
+
+            if (c == ']' && top == '[') {
+                st.pop();
+                continue;
+            }
+
+            if (c == '}' && top == '{') {
+                st.pop();
+                continue;
+            }
+
+            st.push(c);
+        }
+
+        return st.isEmpty();
+    }
+
+    private boolean isOpener(char c) {
+        return OPENER.stream().anyMatch(o -> c == o);
+    }
+}
+
+/* test with Junit5
+public class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @MethodSource("testcase")
+    @ParameterizedTest
+    void test(String s, boolean expected) {
+        var actual = solution.isValid(s);
+//        System.out.println(actual);
+//        System.out.println(expected);
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> testcase() {
+        return Stream.of(
+            Arguments.of("()", true),
+            Arguments.of("()[]{}", true),
+            Arguments.of("(]", false),
+            Arguments.of("([)]", false),
+            Arguments.of("{[]}", true)
+        );
+    }
+}
+ */
