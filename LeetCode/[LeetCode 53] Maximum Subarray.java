b94024d2777593ceda94d71c07c75eeb2420a045
dhmin5693 <dhmin5693@naver.com>
2021-05-13 01:10:39 +0900

    2021-05-13

--- LeetCode/[LeetCode 53] Maximum Subarray.java
+++ LeetCode/[LeetCode 53] Maximum Subarray.java
@@ -1,12 +1,38 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // todo solve this...
-        return null;
+
+        int answer = nums[0];
+        int max = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(nums[i], max + nums[i]);
+            answer = Math.max(max, answer);
+        }
+
+        return answer;
     }
 }
 
 /* test with Junit5
 
-// do something...
+public class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @MethodSource("testcase")
+    @ParameterizedTest
+    void test(int[] nums, int expected) {
+        var actual = solution.maxSubArray(nums);
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> testcase() {
+        return Stream.of(
+            Arguments.of(new int[]{-2,1,-3,4,-1,2,1,-5,4}, 6),
+            Arguments.of(new int[]{1},1),
+            Arguments.of(new int[]{5,4,-1,7,8},23)
+        );
+    }
+}
 
  */
