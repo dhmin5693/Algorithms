c439ac87c16eb79a2dd8208f2c8d8cce22c18e9f
dhmin5693 <dhmin5693@naver.com>
2021-05-15 15:25:24 +0900

    2021-05-15

--- LeetCode/[LeetCode 1235] Maximum Profit in Job Scheduling.java
+++ LeetCode/[LeetCode 1235] Maximum Profit in Job Scheduling.java
@@ -17,12 +17,13 @@ class Solution {
 
         var jobs = Stream.iterate(0, i -> i < startTime.length, i -> i + 1)
                          .map(i -> new Job(startTime[i], endTime[i], profit[i]))
-                         .sorted(Comparator.comparingInt(j -> j.end))
+                         .sorted()
                          .collect(toUnmodifiableList());
 
         for (Job job : jobs) {
             int value = dp.floorEntry(job.start).getValue() + job.profit;
-            if (value > dp.lastEntry().getValue()) {
+
+            if (value > dp.floorEntry(job.end).getValue()) {
                 dp.put(job.end, value);
             }
         }
@@ -31,7 +32,7 @@ class Solution {
                  .getValue();
     }
 
-    public static class Job {
+    public static class Job implements Comparable<Job> {
         int start;
         int end;
         int profit;
@@ -41,11 +42,38 @@ class Solution {
             this.end = end;
             this.profit = profit;
         }
+
+        @Override
+        public int compareTo(Job o) {
+            if (end != o.end) {
+                return end - o.end;
+            }
+            return start - o.start;
+        }
     }
 }
 
 /* test with Junit5
 
+public class SolutionTest {
 
+    private final Solution solution = new Solution();
+
+    @MethodSource("testcase")
+    @ParameterizedTest
+    void test(int[] startTime, int[] endTime, int[] profit, int expected) {
+        var actual = solution.jobScheduling(startTime, endTime, profit);
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> testcase() {
+        return Stream.of(
+            Arguments.of(new int[] {4,2,4,8,2}, new int[] {5,5,5,10,8}, new int[] {1,2,8,10,4}, 18),
+            Arguments.of(new int[] {1,2,3,4,6}, new int[] {3,5,10,6,9}, new int[] {20,20,100,70,60}, 150),
+            Arguments.of(new int[] {1,2,3,3}, new int[] {3,4,5,6}, new int[] {50,10,40,70}, 120),
+            Arguments.of(new int[] {1,1,1}, new int[] {2,3,4}, new int[] {5,6,4}, 6)
+        );
+    }
+}
 
 */
