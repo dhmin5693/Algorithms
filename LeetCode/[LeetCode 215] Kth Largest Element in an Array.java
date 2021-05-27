ffb60fa2e85b6b0ed0c1839e224eb1baaa39ddb6
dhmin5693 <dhmin5693@naver.com>
2021-05-28 00:33:09 +0900

    2021-05-28

--- LeetCode/[LeetCode 215] Kth Largest Element in an Array.java
+++ LeetCode/[LeetCode 215] Kth Largest Element in an Array.java
@@ -1,11 +1,41 @@
+import java.util.PriorityQueue;
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        
+
+        var pq = new PriorityQueue<Integer>();
+        for (int num : nums) {
+            pq.add(-num);
+        }
+
+        for (int i = 0; i < k - 1; i++) {
+            pq.poll();
+        }
+
+        return -1 * pq.poll();
     }
 }
 
+
 /* Test code with Junit 5
 
+public class SolutionTest {
 
+    private final Solution solution = new Solution();
+
+    @MethodSource("testcase")
+    @ParameterizedTest
+    void test(int[] nums, int k, int expected) {
+        var actual = solution.findKthLargest(nums, k);
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> testcase() {
+        return Stream.of(
+            Arguments.of(new int[] {3,2,1,5,6,4}, 2, 5),
+            Arguments.of(new int[] {3,2,3,1,2,4,5,5,6}, 4, 4)
+        );
+    }
+}
 
  */
