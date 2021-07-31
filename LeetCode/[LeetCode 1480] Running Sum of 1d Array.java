e314eb1c9fe335f856d97a3253071b9dc9801e38
dhmin5693 <dhmin5693@naver.com>
2021-07-31 23:43:00 +0900

    2021-07-31

---
+++ LeetCode/[LeetCode 1480] Running Sum of 1d Array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] answer = new int[nums.length];
+        answer[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            answer[i] = answer[i - 1] + nums[i];
+        }
+
+        return answer;
+    }
+}
+
+
+/* Test code with Junit5
+
+public class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @MethodSource("testcase")
+    @ParameterizedTest
+    void test(int[] nums, int[] expected) {
+        var actual = solution.runningSum(nums);
+        assertArrayEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> testcase() {
+        return Stream.of(
+            Arguments.of(new int[] {1,2,3,4}, new int[] {1,3,6,10}),
+            Arguments.of(new int[] {1,1,1,1,1}, new int[] {1,2,3,4,5}),
+            Arguments.of(new int[] {3,1,2,10,1}, new int[] {3,4,6,16,17})
+        );
+    }
+}
+
+ */
